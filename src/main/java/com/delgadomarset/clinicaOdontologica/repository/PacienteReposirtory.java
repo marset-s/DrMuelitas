0c1650475e3856073c72644083c1a5a82bf87fed
Serrana Marset <serranamarset@gmail.com>
2023-06-20 12:59:21 -0300

    Se refactoriza capa repository implemntando JPA

--- src/main/java/com/delgadomarset/clinicaOdontologica/repository/PacienteReposirtory.java
+++ src/main/java/com/delgadomarset/clinicaOdontologica/repository/PacienteReposirtory.java
@@ -2,6 +2,8 @@ package com.delgadomarset.clinicaOdontologica.repository;
 
 import com.delgadomarset.clinicaOdontologica.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PacienteReposirtory extends JpaRepository <Paciente, Long>{
 }
---
+++ src/main/java/com/delgadomarset/clinicaOdontologica/repository/TurnoRepository.java
@@ -0,0 +1,7 @@
+package com.delgadomarset.clinicaOdontologica.repository;
+
+import com.delgadomarset.clinicaOdontologica.entity.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurnoRepository extends JpaRepository<Turno, Long> {
+}
